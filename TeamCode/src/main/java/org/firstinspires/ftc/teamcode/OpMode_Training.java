b13bd3cf3442df30ade8ca35bfa7d1c84da149a8
sagnick <sagnikdps@gmail.com>
2017-08-09 18:54:47 -0700

    Minor Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Training.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Training.java
@@ -36,6 +36,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**

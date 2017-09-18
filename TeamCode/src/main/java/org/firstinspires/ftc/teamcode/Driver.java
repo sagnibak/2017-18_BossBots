825abd9b46276f9ad948b3235268bccee8e190f0
SteveSwihart <steve@swiharts.com>
2017-09-17 22:37:53 -0700

    Updated ASCII diagram with robot orientation and movement directions. Also commented out 3 motors to demo 1-4 incorrect linkage (with 1-3 commented, moving stick drives both 1 and 4).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
@@ -5,19 +5,29 @@ import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by sagnick on 8/18/2017.
+ * Edited by Steve 17-Sep-17
  */
 
 /**
-    *   Motor position
-    *
-    * motor4     motor3
-    *    [/]-------[\]
-    *       |     |
-    *       |     |
-    *       |     |
-    *    [\]-------[/]
-    *  motor1    motor2
-*/
+ *   Motor position (as seen from above)
+ *
+ *       FRONT
+ * motor4     motor3
+ *    [\]-------[/]
+ *       |     |
+ *       |     |
+ *       |     |
+ *    [/]-------[\]
+ *  motor1    motor2
+ *
+ * Direction     X  Y  Actuation
+ *  Forward      0  -   All forward same speed
+ *  Reverse      0  +   All backward same speed
+ *  Right Shift  +  0   Wheels 4 and 1 forward, 3 and 2 backward
+ *  Left Shift   -  0   Wheels 3 and 2 forward, 4 and 1 backward
+ *  CW Turn             Wheels 3 and 1 forward, 4 and 2 backward
+ *  CCW Turn            Wheels 4 and 2 forward, 3 and 1 backward
+ */
 
 public abstract class Driver extends OpMode implements Navigation{
     public double powerMotor1 = 0.0, powerMotor2 = 0.0,
@@ -26,16 +36,16 @@ public abstract class Driver extends OpMode implements Navigation{
 
     public void setMotorPower(float joystickX, float joystickY, float turnStick){
         joystickX = joystickX/powerScale;
-        joystickY = -joystickY/powerScale;  // because pushing the joystick forward maekes the reading more negative
+        joystickY = -joystickY/powerScale;  // because pushing the joystick forward makes the reading more negative
 
         turnStick = Range.clip(turnStick, -1, 1);
 
         if (turnStick == 0) {
             // change the motor numbers after testing
-            powerMotor1 = (joystickY + joystickX);
-            powerMotor2 = (joystickY - joystickX);
-            powerMotor3 = (joystickY + joystickX);
-            powerMotor4 = (joystickY - joystickX);
+            powerMotor1 = 0; //(joystickY + joystickX); // Never moves when only 1 is active
+            powerMotor2 = 0; //(joystickY - joystickX); // Y correct. Once all 4 work, might have to - X
+            powerMotor3 = 0; //(joystickY - joystickX); // Y correct. Once all 4 work, might have to - X
+            powerMotor4 = (joystickY + joystickX);  //Both 1 and 4 move when only 4 is active
         }
 
         if (turnStick != 0) {
